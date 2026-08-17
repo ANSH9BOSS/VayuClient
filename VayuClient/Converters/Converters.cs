bb0ab4b0c8185581fe2af51b87aa5d1c2142d5f2
ANSH9BOSS <ansh9boss@vayuclient.net>
2026-08-17 20:09:43 +0530

    fix(ui): add StringToVisibilityConverter and register in App.xaml

--- VayuClient/Converters/Converters.cs
+++ VayuClient/Converters/Converters.cs
@@ -81,6 +81,15 @@ namespace VayuClient.Converters
             => throw new NotImplementedException();
     }
 
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is string s && !string.IsNullOrWhiteSpace(s) ? Visibility.Visible : Visibility.Collapsed;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
     public class EqualValueToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
