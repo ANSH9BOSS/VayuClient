9e0ecc8fbc26bb7019cdc06c9e5080d22148fbfa
ANSH9BOSS <ansh9boss@vayuclient.net>
2026-09-10 21:26:45 +0530

    chore: remove telemetry caches and scratch test scripts from distribution repository

--- scratch/find_javas.ps1
+++
@@ -1,1 +0,0 @@
-Get-ChildItem -Path "$env:ProgramFiles\Java", "$env:ProgramFiles\Eclipse Adoptium", "$env:ProgramFiles\Microsoft\jdk*", "$env:LOCALAPPDATA\Programs\Common\Oracle\Java", "$env:APPDATA\.minecraft\runtime" -Recurse -Filter "java.exe" -ErrorAction SilentlyContinue | Select-Object FullName
